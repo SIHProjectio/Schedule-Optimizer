7f5fc202bc69a0df11b65806b4d86289b0b38e0f
Arpbansal <arpitbansal761943@gmail.com>
2026-01-21 16:47:16 +0530

    cleaning

--- benchmarks/constraint_satisfaction/test_constraint_benchmark.py
+++ benchmarks/constraint_satisfaction/test_constraint_benchmark.py
@@ -4,7 +4,6 @@ Test Constraint Satisfaction Benchmark
 import sys
 import os
 
-# Add project root to path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 from DataService.generators.enhanced_generator import EnhancedMetroDataGenerator
